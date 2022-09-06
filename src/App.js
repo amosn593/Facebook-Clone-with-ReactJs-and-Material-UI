6d28c8a7426d6518c0082d0dd9642567e4dfb303
Amos Ndonga <amosndonga@gmail.com>
2022-09-06 14:14:51 +0300

    Finished everything

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import Sidebar from "./components/Sidebar";
-import Feed from "./components/Feed";
-import Rightbar from "./components/Rightbar";
-import { Box, createTheme, Stack, ThemeProvider } from "@mui/material";
-import Navbar from "./components/Navbar";
-import Add from "./components/Add";
-import { useState } from "react";
+import Sidebar from './components/Sidebar';
+import Feed from './components/Feed';
+import Rightbar from './components/Rightbar';
+import { Box, createTheme, Stack, ThemeProvider } from '@mui/material';
+import Navbar from './components/Navbar';
+import Add from './components/Add';
+import { useState } from 'react';
 
 function App() {
-  const [mode, setMode] = useState("light");
+  const [mode, setMode] = useState('light');
 
   const darkTheme = createTheme({
     palette: {
@@ -16,10 +16,10 @@ function App() {
   });
   return (
     <ThemeProvider theme={darkTheme}>
-      <Box bgcolor={"background.default"} color={"text.primary"}>
+      <Box bgcolor={'background.default'} color={'text.primary'}>
         <Navbar />
         <Stack direction="row" spacing={2} justifyContent="space-between">
-        <Sidebar setMode={setMode} mode={mode}/>
+          <Sidebar setMode={setMode} mode={mode} />
           <Feed />
           <Rightbar />
         </Stack>
